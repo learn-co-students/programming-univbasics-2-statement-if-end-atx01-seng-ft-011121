cef53fdfdf1dfcb1cf365a194d5eac4cf869ffbc
Jacob Hampton <jhampton792@gmail.com>
2020-11-29 18:10:22 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,16 @@
 # Write your solution here
+
+time = Time.now 
+
+##if time.to_i % 2 == 0 
+##  puts "Even!"
+##else
+##  puts "Odd!"
+##end
+
+##cool trick to know is this .even? method on an integer
+if time.to_i.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
